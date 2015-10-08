c261c92270b37ddf58fe4d56cc8599ca1797446e
kangle yu <atupalykl@gmail.com>
2015-10-08 13:56:06 +0800

    Update ydcv.psm1

--- ydcv.psm1
+++ ydcv.psm1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Web
 
 $wc = New-Object system.Net.WebClient;
-$wc.Encoding = [System.Text.Encoding]::UTF8
+$wc.Encoding = [System.Text.Encoding]::UTF8;
 
 $keyfrom = 'atupal-site';
 $key = '401682907';
@@ -11,24 +11,24 @@ $OutputEncoding = [System.Text.Encoding]::UTF8;
 
 function print_explanation()
 {
-    #[CmdletBinding()]
-    param([Switch] $debug)
-	$obj = $args[0];
+    #[CmdletBinding()];
+    param([Switch] $debug);
+    $obj = $args[0];
     if ($debug -ne $false)
     {
         write-host ($result | ConvertFrom-Json | ConvertTo-Json);
     }
         
-	Write-Host $obj.query -NoNewline -ForegroundColor Red -BackgroundColor Yellow
-    Write-Host " " -NoNewline
+    Write-Host $obj.query -NoNewline -ForegroundColor Red -BackgroundColor Yellow;
+    Write-Host " " -NoNewline;
     if ($obj.basic -ne $null)
     {
         $b = $obj.basic;
-        '[{0} - {1}] ' -f $b."uk-phonetic",$b."us-phonetic" | Write-Host -ForegroundColor Yellow -NoNewline
-        write-host $obj.translation -ForegroundColor Green
+        '[{0} - {1}] ' -f $b."uk-phonetic",$b."us-phonetic" | Write-Host -ForegroundColor Yellow -NoNewline;
+        Write-Host $obj.translation -ForegroundColor Green;
         if ($b.explains -ne "null")
         {
-            Write-Host "  Word Explanation:" -ForegroundColor Cyan
+            Write-Host "  Word Explanation:" -ForegroundColor Cyan;
             foreach ($e in $_b.explains)
             {
                 "    * {0}" -f $e | write-host;
@@ -39,18 +39,18 @@ function print_explanation()
     {
         foreach ($e in $obj.translation)
         {  
-            "    * {0}" -f $e | write-host;
+            "    * {0}" -f $e | Write-Host;
         }
     }
     if ($obj.web -ne $null)
     {
         $w = $obj.web;
-        write-host
-        write-host "  Web Reference:" -ForegroundColor Cyan
+        Write-Host;
+        Write-Host "  Web Reference:" -ForegroundColor Cyan;
         foreach ($o in $w)
         {
-            "    * {0}" -f $o.key | write-host -ForegroundColor Yellow
-            "      {0}" -f [string]::Join(" ", $o.value) | write-host -ForegroundColor Magenta
+            "    * {0}" -f $o.key | Write-Host -ForegroundColor Yellow;
+            "      {0}" -f [string]::Join(" ", $o.value) | Write-Host -ForegroundColor Magenta;
         }
 
     }
@@ -58,18 +58,18 @@ function print_explanation()
 
 function ydcv()
 {
-    param([Switch] $debug)
-	foreach ($arg in $args)
-	{
-	    $query = [System.Web.HttpUtility]::UrlEncode($arg);
-	    $url = 'http://fanyi.youdao.com/openapi.do?keyfrom=' `
+    param([Switch] $debug);
+    foreach ($arg in $args)
+    {
+    	$query = [System.Web.HttpUtility]::UrlEncode($arg);
+    	$url = 'http://fanyi.youdao.com/openapi.do?keyfrom=' `
 	    + $keyfrom + '&key=' `
 	    + $key + '&type=data&doctype=json&version=1.1&q=' + $query;
 	    $result = $wc.DownloadString($url);
 	    $result_object = ($result | ConvertFrom-Json);
-		print_explanation $result_object -debug:$debug.ToBool();
+	    print_explanation $result_object -debug:$debug.ToBool();
 	    #write-host ($result | ConvertFrom-Json | ConvertTo-Json);
-	}
+    }
 }
 
 function speak($word)
@@ -81,6 +81,6 @@ function speak($word)
 
 function sw()
 {
-    Invoke-Expression "ydcv $args"
+    Invoke-Expression "ydcv $args";
     speak $args[0];
 }
