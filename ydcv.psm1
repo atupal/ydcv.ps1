4b79089dcdea1695248dc06280b384b284311325
kangle yu <atupalykl@gmail.com>
2015-10-10 10:37:32 +0800

    add  Get-Clipboard function

--- ydcv.psm1
+++ ydcv.psm1
@@ -14,22 +14,22 @@ function c {
 }
 
 function Get-Clipboard([switch] $Lines) {
-	if($Lines) {
-		$cmd = {
-			Add-Type -Assembly PresentationCore
-			[Windows.Clipboard]::GetText() -replace "`r", '' -split "`n"
-		}
-	} else {
-		$cmd = {
-			Add-Type -Assembly PresentationCore
-			[Windows.Clipboard]::GetText()
-		}
-	}
-	if([threading.thread]::CurrentThread.GetApartmentState() -eq 'MTA') {
-		& powershell -Sta -Command $cmd
-	} else {
-		& $cmd
-	}
+    if($Lines) {
+        $cmd = {
+            Add-Type -Assembly PresentationCore
+            [Windows.Clipboard]::GetText() -replace "`r", '' -split "`n"
+        }
+    } else {
+        $cmd = {
+            Add-Type -Assembly PresentationCore
+            [Windows.Clipboard]::GetText()
+        }
+    }
+    if([threading.thread]::CurrentThread.GetApartmentState() -eq 'MTA') {
+        & powershell -Sta -Command $cmd
+    } else {
+        & $cmd
+    }
 }
 
 function print_explanation()
