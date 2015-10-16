0fc6728e80b611a71655ec0e342edca4ded53813
kangle yu <atupalykl@gmail.com>
2015-10-16 10:12:57 +0800

    Add balloon notification

--- ydcv.psm1
+++ ydcv.psm1
@@ -81,37 +81,121 @@ function print_explanation()
 
 function ydcv()
 {
-    param(
+    #param([Switch] $debug);
+    Param(
         [Switch] $debug,
-        [Switch] $copyMode
+        [Switch] $copyMode,
+        [Switch] $selectMode,
+        [Switch] $balloon
     );
-
-    if ($copyMode -ne $false)
+    
+    function Show-BalloonTip  
     {
-        while ($true)
+        [CmdletBinding(SupportsShouldProcess = $true)]
+        param
+        (
+            [Parameter(Mandatory=$true)]
+            $Text,
+   
+            [Parameter(Mandatory=$true)]
+            $Title,
+   
+            [ValidateSet('None', 'Info', 'Warning', 'Error')]
+            $Icon = 'Info',
+            $Timeout = 10000
+        )
+ 
+        Add-Type -AssemblyName System.Windows.Forms
+
+        if ($script:balloon -eq $null)
         {
-            $currentText = Get-Clipboard;
-            if (($currentText -ne $preText) -and ($currentText -match "^[a-zA-Z ,.-]+$"))
+            $script:balloon = New-Object System.Windows.Forms.NotifyIcon
+        }
+        $path                    = Get-Process -id $pid | Select-Object -ExpandProperty Path
+        $script:balloon.Icon            = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+        $script:balloon.BalloonTipIcon  = $Icon
+        $script:balloon.BalloonTipText  = $Text
+        $script:balloon.BalloonTipTitle = $Title
+        $script:balloon.Visible         = $true
+
+        $script:balloon.ShowBalloonTip($Timeout)
+    }
+
+    #Show-BalloonTip -Text "sefd" -Title "nima" -Icon None -Timeout 5000;
+
+    if ($copyMode -ne $false -or $selectMode -ne $false)
+    {
+
+        Add-Type -AssemblyName System.Windows.Forms;
+        try
+        {
+            while ($true)
             {
-                ydcv $currentText.trim();
-                $preText = $currentText;
+                if ($selectMode -ne $false)
+                {
+                    #[System.Windows.Forms.SendKeys]::SendWait("^c");
+                    # use "<ctrl-ins> but not "<ctrl-c>" to prevent terminal some programs(e.g. script)!
+                    [System.Windows.Forms.SendKeys]::SendWait("^{INS}");
+                    Start-Sleep -Milliseconds 200;
+                }
+                $currentText = Get-Clipboard;
+                if (($currentText -ne $preText) -and ($currentText -match "^[a-zA-Z ,-.]+$"))
+                {
+                    $result_object = ydcv $currentText.trim() -balloon;
+
+                    $title = "title";
+                    $text = "text";
+                    if ($result_object.query -ne $null)
+                    {
+                        $title = $result_object.query+' [{0} - {1}] ' -f $result_object.basic."uk-phonetic",$result_object.basic."us-phonetic" + $result_object.translation;
+                    }
+                    if ($result_object.basic.explains -ne $null)
+                    {
+                        $text = $result_object.basic.explains;
+                    }
+                    elseif ($result_object.translation -ne $null)
+                    {
+                        $text = $result_object.translation
+                    }
+                    Show-BalloonTip -Text $text -Title $title -Icon None -Timeout 5000;
+
+                    $preText = $currentText;
+                }
+                if ($selectMode -eq $false) 
+                {
+                    Start-Sleep -Milliseconds 100;
+                }
             }
-            Start-Sleep -Milliseconds 100;
+        }
+        catch
+        {
+            throw $_;
+        }
+        finally
+        {
+            $script:balloon.Dispose();
+            Remove-Variable -Scope script -Name balloon;
+            Write-Host "Bye!"
         }
 
     }
-    
-    foreach ($arg in $args)
-    {
-    	$query = [System.Web.HttpUtility]::UrlEncode($arg);
-    	$url = 'http://fanyi.youdao.com/openapi.do?keyfrom=' `
+
+	foreach ($arg in $args)
+	{
+	    $query = [System.Web.HttpUtility]::UrlEncode($arg);
+	    $url = 'http://fanyi.youdao.com/openapi.do?keyfrom=' `
 	    + $keyfrom + '&key=' `
 	    + $key + '&type=data&doctype=json&version=1.1&q=' + $query;
 	    $result = $wc.DownloadString($url);
 	    $result_object = ($result | ConvertFrom-Json);
-	    print_explanation $result_object -debug:$debug.ToBool();
+		print_explanation $result_object -debug:$debug.ToBool();
 	    #write-host ($result | ConvertFrom-Json | ConvertTo-Json);
-    }
+
+        if ($balloon -ne $false)
+        {
+            return $result_object;
+        }
+	}
 }
 
 function speak($word)
